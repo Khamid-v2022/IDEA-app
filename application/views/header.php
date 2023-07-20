d8529c733b0f668589fb8f67a7b51c9944c30dbf
Khamid-v2022 <khamid.webdev@gmail.com>
2023-07-20 15:20:35 +0800

    change the favicon, and title

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'>
-    <title>Idea</title>
+    <title>Boxboard</title>
      
     <link href="<?=base_url()?>assets/plugin/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
     <link href="<?=base_url()?>assets/plugin/css/icons/fontawesome/styles.min.css" rel="stylesheet" type="text/css">
